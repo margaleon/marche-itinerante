7214451c541a99fb26cf16b0fa29511ecd72ee81
Margal <margaleon@tutanota.com>
2022-04-06 18:51:53 +0200

    updated server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get("/", async (req, res) => {
       /* create arrays of images */
       const postIndex = Number(index) + 1;
       let imagesArray = [];
-      const dir = __dirname + `/views/static/uploads/posts/post_${postIndex}`;
+      // const dir = __dirname + `/views/static/uploads/posts/post_${postIndex}`;
 
       // try {
       //   const files = fs.readdirSync(dir);
