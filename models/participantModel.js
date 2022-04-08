bdf8654e9da5ada9035dafc1a83658b4d73d79dd
Margal <margaleon@tutanota.com>
2022-04-08 14:02:23 +0200

    updated participants model

--- models/participantModel.js
+++ models/participantModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const participantSchema = new Schema({
   firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  age: { type: Number, required: true },
   presentation: { type: String, required: true },
 });
 
